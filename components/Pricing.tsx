bbf71ef2246fec14354fc4af4b9dff6b99f9e93a
Aiji Uejima <aiji42@gmail.com>
2021-05-05 17:01:03 +0900

    価格情報表示

---
+++ components/Pricing.tsx
@@ -0,0 +1,27 @@
+import { FC } from "react";
+import { PricingItem } from "../types/site";
+import Image from 'next/image'
+
+export const Pricing: FC<PricingItem> = ({ title, body, image }) => {
+  return (
+    <div className="container px-6 py-10 mx-auto md:py-16">
+      <div className="flex flex-col space-y-6 md:flex-row md:items-center md:space-x-6">
+        {image && (
+          <div className="flex items-center justify-center w-full md:w-1/2">
+            <div className="w-full h-full max-w-2xl rounded">
+              <Image src={image.url} alt={title} layout="responsive" width={image.width} height={image.height} />
+            </div>
+          </div>
+        )}
+        <div className="w-full md:w-1/2">
+            <div className="max-w-md mx-auto">
+              <h3 className="text-2xl font-medium tracking-wide text-gray-800 md:text-4xl">
+                {title}
+              </h3>
+              <div className="mt-8" dangerouslySetInnerHTML={{ __html: body }} />
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,28 +5,33 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"
 import { Carousel } from 'react-responsive-carousel'
 import Head from 'next/head'
 import Image from 'next/image'
+import { Pricing } from '../components/Pricing'
 
 interface TopProps {
   data: Site
 }
 
 const Top: FC<TopProps> = ({ data }) => {
-  console.log(data)
-  return (<>
-    <Head>
-      <title>{data.meta.title}</title>
-    </Head>
-    <Carousel showArrows={false} showStatus={false} showThumbs={false} infiniteLoop>
-      {data.mainVisuals.map(({ photo }, index) => (<Image key={index} src={photo.url} width={photo.width} height={photo.height} layout="responsive" />))}
-    </Carousel>
-    <h1 className="mt-8 text-center text-lg">作例</h1>
-    {data.products.map(({ title, photos }, index) => (
-      <Fragment key={index}>
-        <h2>{title}</h2>
-        {photos.map(({ photo }, index) => (<Image key={index} src={photo.url} width={photo.width} height={photo.height} layout="responsive" />))}
-      </Fragment>
-    ))}
-  </>)
+  return (
+    <>
+      <Head>
+        <title>{data.meta.title}</title>
+      </Head>
+      <Carousel showArrows={false} showStatus={false} showThumbs={false} infiniteLoop>
+        {data.mainVisuals.map(({ photo }, index) => (<Image key={index} src={photo.url} width={photo.width} height={photo.height} layout="responsive" />))}
+      </Carousel>
+      <h2 className="mt-8 text-center text-lg">作例</h2>
+      {data.products.map(({ title, photos }, index) => (
+        <Fragment key={index}>
+          <h3>{title}</h3>
+          {photos.map(({ photo }, index) => (<Image key={index} src={photo.url} width={photo.width} height={photo.height} layout="responsive" />))}
+        </Fragment>
+      ))}
+      <h2 className="mt-8 text-center text-lg">価格</h2>
+      {data.pricings.pricings.map((pricing, index) => (<Pricing {...pricing} key={index} />))}
+      {data.pricings.note && <div dangerouslySetInnerHTML={{ __html: data.pricings.note }} />}
+    </>
+  )
 }
 
 export default Top
