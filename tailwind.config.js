eb8d956a69eb508fac9a46fd3db84e9a2f733105
Aiji Uejima <aiji42@gmail.com>
2021-05-05 22:04:37 +0900

    tailwind.config を変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./components/**/*.tsx', './pages/**/*.tsx', './public/**/*.html'],
+  purge: ['./src/**/*.tsx', './public/**/*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
