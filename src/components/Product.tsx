5ddd166959ba3073263979205cc4f66256a2e291
Aiji Uejima <aiji42@gmail.com>
2021-06-12 18:36:53 +0900

    wip

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,4 +1,4 @@
-import { ClassAttributes, FC, useRef, useState } from 'react'
+import { FC, useRef, useState } from 'react'
 import { Product as ProductType } from '../types/site'
 import Image from 'next/image'
 import Masonry from 'react-masonry-css'
@@ -24,7 +24,7 @@ export const Product: FC<ProductType> = ({ title, photos }) => {
           {photos.map(({ photo }, index) => (
             <div
               key={index}
-              className={`px-2 sm:px-1 py-1 ${index > 2 && 'hidden sm:block'}`}
+              className={`px-2 sm:px-1 py-1 ${index > 3 && 'hidden sm:block'}`}
             >
               <Image
                 src={photo.url}
@@ -36,12 +36,12 @@ export const Product: FC<ProductType> = ({ title, photos }) => {
             </div>
           ))}
         </Masonry>
-        <div className="text-center">
+        <div className="text-center py-2 sm:hidden">
           <button
             onClick={() => {
               ref.current?.fullScreen()
             }}
-            className="inline-block px-6 py-2 text-xs font-medium leading-6 text-center text-blue-700 uppercase transition bg-transparent border-2 border-blue-700 rounded-full ripple hover:bg-blue-100 focus:outline-none waves-effect"
+            className="inline-block px-6 py-2 text-xs font-medium leading-6 text-center text-gray-700 uppercase transition bg-transparent border border-gray-700 rounded-full ripple hover:bg-gray-100 focus:outline-none waves-effect font-serif"
           >
             See More
           </button>
@@ -50,12 +50,12 @@ export const Product: FC<ProductType> = ({ title, photos }) => {
           <ImageGallery
             ref={ref}
             lazyLoad
-            showThumbnails={false}
-            startIndex={2}
+            startIndex={3}
             showPlayButton={false}
             showBullets
             items={photos.map(({ photo }) => ({
               original: photo.url,
+              thumbnail: photo.url,
               originalHeight: photo.height,
               originalWidth: photo.width
             }))}
