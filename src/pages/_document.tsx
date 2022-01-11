658c8e7c8b9ac07cfebcd552cde38e06f2b06367
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-01-11 23:35:54 +0900

    GTMを環境変数に応じて出し分ける

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -21,6 +21,23 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Cormorant&display=swap"
             rel="stylesheet"
           />
+          {process.env.NEXT_PUBLIC_GTM_ID &&
+            <>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GTM_ID}');`
+                }}
+              />
+            </>
+            }
         </Head>
         <body>
           <Main />
