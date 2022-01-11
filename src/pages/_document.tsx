50fedb74024ee9d5913638584a8f8bfd58734425
AijiUejima <aiji42@gmail.com>
2022-01-11 23:19:37 +0900

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -21,20 +21,6 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Cormorant&display=swap"
             rel="stylesheet"
           />
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-DD9QN60LWX"
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-DD9QN60LWX');`
-            }}
-          />
         </Head>
         <body>
           <Main />
