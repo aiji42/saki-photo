cd3d7f8c02eab907ee3c10635794daf5024f74e8
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2023-12-01 16:30:43 +0900

    update form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,14 +1,10 @@
 import { FC } from 'react'
-
-const script = `!function n(o,i,a){function u(t,e){if(!i[t]){if(!o[t]){var r="function"==typeof require&&require;if(!e&&r)return r(t,!0);if(d)return d(t,!0);throw(e=new Error("Cannot find module '"+t+"'")).code="MODULE_NOT_FOUND",e}r=i[t]={exports:{}},o[t][0].call(r.exports,function(e){return u(o[t][1][e]||e)},r,r.exports,n,o,i,a)}return i[t].exports}for(var d="function"==typeof require&&require,e=0;e<a.length;e++)u(a[e]);return u}({1:[function(e,t,r){"use strict";var n=[];function o(){Array.prototype.forEach.call(document.querySelectorAll(".formrun-embed"),function(e){var t,r;"true"==(e=e).dataset.rendered&&0!=e.childElementCount||((t=e.dataset.formrunForm)||console.error("data-formrun-form is not set."),/@[^\\/]+/.test(t)||console.error("data-formrun-form is invalid: "+t),t="https://form.run/embed/"+t,(r=document.createElement("iframe")).setAttribute("src",t),r.setAttribute("frameborder","no"),r.setAttribute("loading","lazy"),r.style.backgroundColor="#FFFFFF",r.style.width="100%",r.style.height="0","true"===e.dataset.formrunRedirect&&r.addEventListener("load",function(){r.contentWindow.postMessage({formrunRedirect:!0},"*")}),e.appendChild(r),n.push(r),e.dataset.rendered="true")})}window.addEventListener("message",function(t){var r;"https://form.run"==t.origin&&null!=(r=function(e){try{return JSON.parse(e)}catch(e){}}(t.data))&&("scrollTop"==r.action&&n.forEach(function(e){e.scrollIntoView(!0)}),r.height&&n.forEach(function(e){0<=e.getAttribute("src").indexOf(r.friendlyKey)&&t.source===e.contentWindow&&("thanks"===r.page&&e.style.height!==r.height+"px"&&window.scrollTo(0,e.offsetTop),e.style.display="block",e.style.height=r.height+"px")}))},!1),"loading"==document.readyState?document.addEventListener("DOMContentLoaded",o,!1):o()},{}]},{},[1]);`
+import Script from 'next/script'
 
 export const Form: FC = () => {
   return (
     <>
-      <script
-        type="text/javascript"
-        dangerouslySetInnerHTML={{ __html: script }}
-      />
+      <Script src="/form.js" />
       <div className="mt-8 formrun-embed" data-formrun-form="@saki-photo" />
     </>
   )
